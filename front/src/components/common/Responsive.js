96a8055fc8e34d5916423b0cbfcd87af51d9a88d
suminlee <suminpixel@gmail.com>
2020-07-17 23:43:00 +0900

    브랜치를 위한 중간 업로드

---
+++ front/src/components/common/Responsive.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Styled from 'styled-components';
+
+
+const ResponsiveBlock = Styled.div`
+    padding-left: 1rem;
+    padding-right: 1rem;
+    width: 1024px;
+    margin: 0 auto;
+`
